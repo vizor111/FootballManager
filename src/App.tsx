06ae6620aa684e058042181ed1ed1357d76c596a
Ivan Nichipor <ivan.nichipor@effective-soft.com>
2024-11-05 19:00:20 +0300

    Start working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import teams from './teams';
 
 function App() {
+  const [selectedTeams, setSelectedTeams] = useState<Record<string, boolean>>(
+    teams.reduce((acc, team) => ({ ...acc, [team.Name]: false }), {})
+  );
+
+  const handleCheckboxChange = (teamName: string) => {
+    const selectedCount = Object.values(selectedTeams).filter(Boolean).length;
+    const isCurrentlySelected = selectedTeams[teamName];
+
+    if (!isCurrentlySelected && selectedCount >= 2)
+       return;
+
+    setSelectedTeams((prevState) => ({
+      ...prevState,
+      [teamName]: !prevState[teamName]
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="table">
+      <div className="row heading">
+        <div className="cell">Team</div>
+        <div className="cell">Rating</div>
+        <div className="cell">Selector</div>
+      </div>
+      {teams.map((team, index) => (
+        <div className="row" key={index}>
+          <div className="cell">{team.Name}</div>
+          <div className="cell">{team.Rating}</div>
+          <div className="cell">
+            <input
+              type="checkbox"
+              checked={selectedTeams[team.Name]}
+              onChange={() => handleCheckboxChange(team.Name)}
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/teams.ts
@@ -0,0 +1,9 @@
+const teams = [
+    { Name: "Arsenal", Rating: 4.5 },
+    { Name: "Chelsea", Rating: 4.0 },
+    { Name: "MU", Rating: 3.9 },
+    { Name: "MC", Rating: 5.0 },
+    { Name: "Liverpool", Rating: 4.5 }
+  ];
+  
+  export default teams;
